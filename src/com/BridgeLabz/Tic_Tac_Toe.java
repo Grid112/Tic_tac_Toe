d43d1d68f199b7cbdf8496322d549b7ca99803e3
Grid112 <sumitware1999@gmail.com>
2021-09-29 19:53:39 +0530

    Added A method So That User Can Move To The Desired Location On Game Board

--- src/com/BridgeLabz/Tic_Tac_Toe.java
+++ src/com/BridgeLabz/Tic_Tac_Toe.java
@@ -11,6 +11,29 @@ public class Tic_Tac_Toe {
             System.out.println();
         }
     }
+    public static void placePiece(char[][] gameBoard,int pos, char letter){
+        switch (pos){
+            case 1: gameBoard[0][0]= letter;
+                break;
+            case 2: gameBoard[0][2] = letter;
+                break;
+            case 3: gameBoard[0][4] = letter;
+                break;
+            case 4: gameBoard[2][0] = letter;
+                break;
+            case 5: gameBoard[2][2] = letter;
+                break;
+            case 6: gameBoard[2][4] = letter;
+                break;
+            case 7: gameBoard[4][0] = letter;
+                break;
+            case 8: gameBoard[4][2] = letter;
+                break;
+            case 9: gameBoard[4][4] = letter;
+                break;
+        }
+        printGameBoard(gameBoard);
+    }
     public static void main(String args[]) {
         Scanner SC = new Scanner(System.in);
         System.out.println("Welcome to the tic tac toe");
@@ -20,7 +43,10 @@ public class Tic_Tac_Toe {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}};
         printGameBoard(gameBoard);
-        int pos = SC.nextInt();
+        System.out.println("Enter the letter X or o");
         char letter = SC.next().charAt(0);
+        System.out.println("Enter the position between 1 to 9");
+        int pos = SC.nextInt();
+        placePiece(gameBoard,pos,letter);
     }
 }
