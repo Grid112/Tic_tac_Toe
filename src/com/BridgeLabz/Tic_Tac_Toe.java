fa136be08758998b001988d95e845c0cdc31c83b
Grid112 <sumitware1999@gmail.com>
2021-09-29 19:29:36 +0530

    Using a Meathod To Create A GAme Board using Array

--- src/com/BridgeLabz/Tic_Tac_Toe.java
+++ src/com/BridgeLabz/Tic_Tac_Toe.java
@@ -1,8 +1,21 @@
 package com.BridgeLabz;
 
 public class Tic_Tac_Toe {
-
-   public static void main(String[] args){
-       System.out.println("Welcome To Tic_Tac_Toe game");
-   }
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        System.out.println("Welcome to the tic tac toe");
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+        printGameBoard(gameBoard);
+    }
 }
